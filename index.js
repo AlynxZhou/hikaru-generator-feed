0f7c2d709007ecd9ff2175519dbd4f870562a255
Alynx Zhou <alynx.zhou@gmail.com>
2019-10-31 13:32:54 +0800

    Updated to use Decorator.

--- index.js
+++ index.js
@@ -2,25 +2,18 @@ const fs = require('fs')
 const path = require('path')
 const nunjucks = require('nunjucks')
 module.exports = (hikaru) => {
-  const {removeControlChars, getURLFn, getPathFn} = hikaru.utils
+  const {removeControlChars} = hikaru.utils
   const {File} = hikaru.types
+  hikaru.decorator.register('atom', path.join(__dirname, 'atom.njk'))
   hikaru.generator.register('rss feed', (site) => {
     if (!site['siteConfig']['feed']['enable']) {
       return
     }
-    const tmpContent = fs.readFileSync(path.join(__dirname, 'atom.njk'), 'utf8')
-    const content = nunjucks.renderString(tmpContent, {
-      'siteConfig': site['siteConfig'],
-      'themeConfig': site['themeConfig'],
-      'posts': site['posts'],
-      'removeControlChars': removeControlChars,
-      'getURL': getURLFn(site['siteConfig']['baseURL'], site['siteConfig']['rootDir']),
-      'getPath': getPathFn(site['siteConfig']['rootDir'])
-    })
     return new File({
       'docDir': site['siteConfig']['docDir'],
       'docPath': site['siteConfig']['feed']['path'] || 'atom.xml',
-      'content': content
+      'layout': 'atom',
+      'removeControlChars': removeControlChars,
     })
   })
 }
