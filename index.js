3dfe7efad8537f3c3773f692cd015875d7022c25
Alynx Zhou <alynx.zhou@gmail.com>
2020-08-06 17:44:12 +0800

    Updated to use standardx for coding style checking.

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
-const path = require('path')
-const pkg = require('./package.json')
+const path = require("path");
+const pkg = require("./package.json");
 
 module.exports = async (hikaru) => {
-  const {removeControlChars} = hikaru.utils
-  const {File} = hikaru.types
-  const fn = await hikaru.compiler.compile(path.join(__dirname, 'atom.njk'))
-  hikaru.decorator.register('atom', fn)
-  hikaru.generator.register('atom feed', (site) => {
-    if (!site['siteConfig']['feed']['enable']) {
-      return
+  const {removeControlChars} = hikaru.utils;
+  const {File} = hikaru.types;
+  const fn = await hikaru.compiler.compile(path.join(__dirname, "atom.njk"));
+  hikaru.decorator.register("atom", fn);
+  hikaru.generator.register("atom feed", (site) => {
+    if (!site["siteConfig"]["feed"]["enable"]) {
+      return;
     }
     return new File({
-      'docDir': site['siteConfig']['docDir'],
-      'docPath': site['siteConfig']['feed']['path'] || 'atom.xml',
-      'layout': 'atom',
-      'removeControlChars': removeControlChars,
-      'generatorVersion': pkg['version']
-    })
-  })
-}
+      "docDir": site["siteConfig"]["docDir"],
+      "docPath": site["siteConfig"]["feed"]["path"] || "atom.xml",
+      "layout": "atom",
+      "removeControlChars": removeControlChars,
+      "generatorVersion": pkg["version"]
+    });
+  });
+};
