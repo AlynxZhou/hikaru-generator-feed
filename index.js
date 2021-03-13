7a8e92c576f0d39eab01c6989f9ebf48d74cea88
Alynx Zhou <alynx.zhou@gmail.com>
2021-03-13 09:36:47 +0800

    Updated to not register if disabled.

--- index.js
+++ index.js
@@ -2,14 +2,14 @@ const path = require("path");
 const pkg = require("./package.json");
 
 module.exports = async (hikaru) => {
+  if (!hikaru.site["siteConfig"]["feed"]["enable"]) {
+    return;
+  }
   const {escapeHTML} = hikaru.utils;
   const {File} = hikaru.types;
   const fn = await hikaru.compiler.compile(path.join(__dirname, "atom.njk"));
   hikaru.decorator.register("atom", fn);
   hikaru.generator.register("atom feed", (site) => {
-    if (!site["siteConfig"]["feed"]["enable"]) {
-      return;
-    }
     return new File({
       "docDir": site["siteConfig"]["docDir"],
       "docPath": site["siteConfig"]["feed"]["path"] || "atom.xml",
