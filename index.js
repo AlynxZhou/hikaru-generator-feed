11debea0f07f55a798df075edfaf5adb5d1d9f35
Alynx Zhou <alynx.zhou@gmail.com>
2022-12-14 22:12:28 +0800

    Fixed version in template.

--- index.js
+++ index.js
@@ -9,22 +9,23 @@ const generateFeed = async (hikaru) => {
   }
   const {escapeHTML, loadJSON} = hikaru.utils;
   const {File} = hikaru.types;
-  const pkgJSON = loadJSON(path.join(pluginDir, "package.json"));
+  const pkgJSON = await loadJSON(path.join(pluginDir, "package.json"));
   const filepath = path.join(pluginDir, "atom.njk");
   const content = await fs.readFile(filepath, "utf8");
   const fn = await hikaru.compiler.compile(filepath, content);
   hikaru.decorator.register("atom", fn, {
-    "dirname": pluginDir, "pathSep": path.sep
+    "dirname": pluginDir,
+    "pathSep": path.sep,
+    "getFeedGeneratorVersion": () => {
+      return pkgJSON["version"];
+    }
   });
   hikaru.generator.register("atom feed", (site) => {
     return new File({
       "docDir": site["siteConfig"]["docDir"],
       "docPath": site["siteConfig"]["feed"]["path"] || "atom.xml",
       "layout": "atom",
-      "escapeHTML": escapeHTML,
-      "getFeedGeneratorVersion": () => {
-        return pkgJSON["version"];
-      }
+      "escapeHTML": escapeHTML
     });
   });
 };
